ae31d6a2af5a2e107a04cac04d485473807eb831
teknokrat-barra <mzonnabarrapratama@teknokrat.ac.id>
2024-01-24 23:47:40 +0700

    master: menambahkan kontak

--- Program/assets/js/main.js
+++ Program/assets/js/main.js
@@ -1,5 +1,19 @@
-
+// tombol function ke kewirausahaahn.html
 document.getElementById("btn_daftar").onclick = function (){
     location.href = "kewirausahaan.html";
 }
 
+// tombol function ke mengunjungi website resmi sekolah 
+// SMK Negeri 1 Bandar Lampung
+
+// Get the button element
+const button = document.getElementById("web_sekolah");
+
+// Set the URL of the website you want to visit
+const url = "https://smkn1bdl.sch.id/";
+
+// Add an event listener to the button
+button.addEventListener("click", () => {
+  // Redirect the user to the URL when the button is clicked
+  window.location.href = url;
+});
